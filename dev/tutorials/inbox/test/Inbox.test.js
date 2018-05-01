c61a4dfb784e066ecd8b50e51e69fb9f4280646e
StreetList Developer <github.com@streetlist.com>
2018-05-01 11:36:53 +0000

    test 4

--- dev/tutorials/inbox/test/Inbox.test.js
+++ dev/tutorials/inbox/test/Inbox.test.js
@@ -2,7 +2,55 @@ const assert = require('assert'); //assertions about test
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider()); 
+const { interface, bytecode } = require('../compile');
 
+
+
+/*
+class Car {
+	park() {
+		return 'stopped';
+	}
+
+	drive() {
+		return 'vroom';
+	}
+}
+
+let car;
+*/
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+	accounts = await web3.eth.getAccounts();
+//	.then(fetchedAccounts => {
+//	console.log(fetchedAccounts);
+//	});
+	inbox = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode, arguments: ['Hi there!'] })
+		.send({ from: accounts[0], gas: '1000000' })
+});
+describe('Inbox', () => {
+	it('deploys a contract', () => {
+	//	const car = new Car();
+	//	assert.equal(car.park(),'stopped');	
+		console.log(inbox)
+	});
+	/*
+	it('can drive', () => {
+	//	const car = new Car();
+		assert.equal(car.drive(),'vroom');
+	});
+	*/
+});
+
+
+
+
+
+
+/*
 class Car {
 	park() {
 		return 'stopped';
@@ -15,7 +63,7 @@ class Car {
 
 let car;
 
-beforeEach() => {
+beforeEach(() => {
 	car = new Car();
 });
 describe('Car', () => {
@@ -29,3 +77,4 @@ describe('Car', () => {
 	});
 
 });
+*/
